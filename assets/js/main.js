99c3bd16baae9c0fdb47607001082bb8d34bd011
Kris Marshall <rflctnofu@hotmail.com>
2022-07-08 12:46:49 -0400

    got cover to display to page

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,38 +45,55 @@ document.addEventListener('DOMContentLoaded', () => {
 // http://openlibrary.org/search.json?
 // https://covers.openlibrary.org/b/id/
 
-// let booksUrl = 'http://openlibrary.org/search.json?q=the+wizard+of+oz&limit=5'
-
-// let body = $('body')
-
-// let getBooks = function () {
-//     fetch(booksUrl)
-//         .then(function (response) {
-//             if (response.ok) {
-//                 console.log(response);
-//                 response.json().then(function (data) {
-//                     console.log(data);
-//                     displayImg(data);
-//                 })
-//             }
-//         })
-// }
-
-// let displayImg = function (image) {
-//     let imgUrl = 'https://covers.openlibrary.org/b/id/' + image.docs[0].cover_i + '-M.jpg';
-
-//     fetch(imgUrl)
-//         .then(function (response) {
-//             console.log(response)
-//             let image = document.createElement('img');
-//             image.setAttribute('src', response.url)
-//             body.prepend(image);
-//             // response.json()
-//             // .then(function (data) {
-//             //     console.log(data);
-//             // })
-//         })
-// }
 
 
+let body = $('body')
+
+
+
+let searchQuery = $('#search-bar-input')
+
+let searchBtn = $('#search-button')
+
+let searchToApi = function (e) {
+    e.preventDefault();
+    console.log("hello")
+    let searchTerm = searchQuery.val();
+    if (searchTerm) {
+        getBooks(searchTerm)
+    }
+
+}
+
+let getBooks = function (search) {
+    let booksUrl = 'http://openlibrary.org/search.json?q=' + search + '&limit=1'
+    fetch(booksUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayImg(data);
+                })
+            }
+        })
+}
+
+let displayImg = function (image) {
+    let imgUrl = 'https://covers.openlibrary.org/b/id/' + image.docs[0].cover_i + '-M.jpg';
+
+    fetch(imgUrl)
+        .then(function (response) {
+            console.log(response)
+            let image = document.createElement('img');
+            image.setAttribute('src', response.url)
+            body.prepend(image);
+            // response.json()
+            // .then(function (data) {
+            //     console.log(data);
+            // })
+        })
+}
+
+searchBtn.click(searchToApi)
 // getBooks();
